871127a145499a88306959c07655b9142f8d4d35
KungD <david@krawaller.se>
2015-12-30 08:07:49 +0100

    added sorted courses and subjects by school

--- src/buildappdata/buildappdata.js
+++ src/buildappdata/buildappdata.js
@@ -9,6 +9,14 @@ var ret = {
   subjectsByCode: {}
 };
 
+function sortKeysByName(db){
+  return Object.keys(db).sort(function(c1,c2){
+    var n1 = db[c1].name.toLowerCase(),
+        n2 = db[c2].name.toLowerCase();
+    return n1 > n2 ? 1 : -1;
+  });
+}
+
 _.each(master.courses,function(course,code){
   ret.coursesByCode[code] = _.extend(_.pick(course,["name","code"]),{
     description: prerender.courseDescription(code),
@@ -26,16 +34,19 @@ _.each(master.subjects,function(subject,code){
   });
 });
 
-ret.sortedcourses = Object.keys(master.courses).sort(function(c1,c2){
-  var n1 = master.courses[c1].name.toLowerCase(),
-      n2 = master.courses[c2].name.toLowerCase();
-  return n1 > n2 ? 1 : -1;
-});
-
-ret.sortedsubjects = Object.keys(master.subjects).sort(function(c1,c2){
-  var n1 = master.subjects[c1].name.toLowerCase(),
-      n2 = master.subjects[c2].name.toLowerCase();
-  return n1 > n2 ? 1 : -1;
-});
+ret.sortedcourses = sortKeysByName(ret.coursesByCode);
+ret.sortedsubjects = sortKeysByName(ret.subjectsByCode);
+ret.sortedcoursesbyschool = ["gymn","grund","grundvux"].reduce(function(mem,school){
+  mem[school] = sortKeysByName(_.omit(ret.coursesByCode,function(c){
+    return c.school !== school;
+  }));
+  return mem;
+},{});
+ret.sortedsubjectsbyschool = ["gymn","grund","grundvux"].reduce(function(mem,school){
+  mem[school] = sortKeysByName(_.omit(ret.subjectsByCode,function(s){
+    return s.school !== school;
+  }));
+  return mem;
+},{});
 
 fs.writeFile("../app/www/js/data.json",JSON.stringify(ret));
