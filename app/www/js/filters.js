03e46dd927e532857322aea345c9bed9e621b610
Jacob Waller <jaco@spotify.com>
2016-01-05 19:00:23 +0100

    Highlight filter substring match in subjects

---
+++ app/www/js/filters.js
@@ -0,0 +1,10 @@
+angular.module('app.filters', [])
+
+.filter('highlight', function($sce) {
+  return function(text, phrase) {
+    if (phrase) text = text.replace(new RegExp('('+phrase+')', 'gi'),
+      '<em>$1</em>')
+
+    return $sce.trustAsHtml(text)
+  }
+})
