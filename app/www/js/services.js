69ac6c4d7a63bdb57c6c8b09074c14316c9618ad
David Waller <david@krawaller.se>
2016-02-10 08:12:27 +0100

    refactored services to use service factory syntax

--- app/www/js/services.js
+++ app/www/js/services.js
@@ -1,69 +1,58 @@
 angular.module('app.services', [])
 
 
-.service('DataService', [function(){
+.factory('DataService', [function(){
 
   var request = new XMLHttpRequest();
   request.open('GET', 'js/data.json', false);
   request.send(null);
 
   var data = JSON.parse(request.responseText);
-  console.log('data', data);
 
-
-  this.sortedsubjects = data.sortedsubjects;
-
-  this.sortedsubjectsbyschool = data.sortedsubjectsbyschool;
-
-  this.sortedgymnsubjectsbytype = data.sortedgymnsubjectsbytype;
-
-  this.sortedcourses = data.sortedcourses;
-
-  this.sortedcoursesbyschool = data.sortedcoursesbyschool;
-
-  this.listAllSubjects = function () {
-    return sortedsubjects;
-    return data.gysubjects.map(function (subjectKey) {
-      return data.subjects[subjectKey];
-    }).sort(function (a, b) {
-      return a.name > b.name ? 1 : (a.name == b.name ? 0 : -1);
-    });
-  };
-
-
-  this.listAllCourses = function () {
-    return sortedcourses;
-    return data.gycourses.map(function (subjectKey) {
-      return data.courses[subjectKey];
-    }).sort(function (a, b) {
-      return a.name > b.name ? 1 : (a.name == b.name ? 0 : -1);
-    });
-  };
-
-  this.getCourse = function (code) {
-    return data.coursesByCode[code];
-  };
-
-  this.getSubject = function (code) {
-    return data.subjectsByCode[code];
-  };
 
   var schooltype = localStorage.getItem('schooltype') || "gymn";
 
-  this.setSchool = function(type){
-    schooltype = type;
-    localStorage.setItem('schooltype',type);
+  return {
+    sortedsubjects: data.sortedsubjects,
+    sortedsubjectsbyschool: data.sortedsubjectsbyschool,
+    sortedgymnsubjectsbytype: data.sortedgymnsubjectsbytype,
+    sortedcourses: data.sortedcourses,
+    sortedcoursesbyschool: data.sortedcoursesbyschool,
+    listAllSubjects: function () {
+      return data.sortedsubjects;
+      /*return data.gysubjects.map(function (subjectKey) {
+        return data.subjects[subjectKey];
+      }).sort(function (a, b) {
+        return a.name > b.name ? 1 : (a.name == b.name ? 0 : -1);
+      });*/
+    },
+    listAllCourses: function () {
+      return data.sortedcourses;
+      /*return data.gycourses.map(function (subjectKey) {
+        return data.courses[subjectKey];
+      }).sort(function (a, b) {
+        return a.name > b.name ? 1 : (a.name == b.name ? 0 : -1);
+      });*/
+    },
+    getCourse: function (code) {
+      return data.coursesByCode[code];
+    },
+    getSubject: function (code) {
+      return data.subjectsByCode[code];
+    },
+    setSchool: function(type){
+      schooltype = type;
+      localStorage.setItem('schooltype',type);
+    },
+    getSchool: function(){
+      return schooltype;
+    }
   };
-
-  this.getSchool = function(){
-    return schooltype;
-  };
-
 }])
 
 
 
-.service('FavouriteService', ["DataService",function(DataService){
+.factory('FavouriteService', ["DataService",function(DataService){
 
   var favorites;
 
@@ -77,31 +66,31 @@ angular.module('app.services', [])
     };
   }
 
-  this.getFavorites = function () {
-    return {
-      courses: favorites.courses.map(function (code) {
-        return DataService.getCourse(code);
-      }),
-      subjects: favorites.subjects.map(function (code) {
-        return DataService.getSubject(code);
-      })
-    };
+  return {
+    getFavorites: function () {
+      return {
+        courses: favorites.courses.map(function (code) {
+          return DataService.getCourse(code);
+        }),
+        subjects: favorites.subjects.map(function (code) {
+          return DataService.getSubject(code);
+        })
+      };
+    },
+    isFavorite: function (code, type) {
+      return favorites[type + 's'].indexOf(code) !== -1;
+    },
+    addFavorite: function (code, type) {
+      var index = favorites[type + 's'].indexOf(code);
+      if (index === -1) favorites[type + 's'].push(code);
+      localStorage.setItem('favorites', JSON.stringify(favorites));
+    },
+    removeFavorite: function (code, type) {
+      var index = favorites[type + 's'].indexOf(code);
+      if (index !== -1) favorites[type + 's'].splice(index, 1);
+      localStorage.setItem('favorites', JSON.stringify(favorites));
+    }
   };
 
-  this.isFavorite = function (code, type) {
-    return favorites[type + 's'].indexOf(code) !== -1;
-  };
-
-  this.addFavorite = function (code, type) {
-    var index = favorites[type + 's'].indexOf(code);
-    if (index === -1) favorites[type + 's'].push(code);
-    localStorage.setItem('favorites', JSON.stringify(favorites));
-  };
-
-  this.removeFavorite = function (code, type) {
-    var index = favorites[type + 's'].indexOf(code);
-    if (index !== -1) favorites[type + 's'].splice(index, 1);
-    localStorage.setItem('favorites', JSON.stringify(favorites));
-  };
 }]);
 
